c34b4552c70796ee2331b1b51591b64bda2f4981
matsuhisa <matsuhisa+github@gmail.com>
2016-03-06 23:55:32 +0900

    Use engines/new_books

--- app/engines/new_books/lib/new_books/engine.rb
+++ app/engines/new_books/lib/new_books/engine.rb
@@ -3,7 +3,6 @@ require 'motorhead/engine'
 module NewBooks
   class Engine < ::Rails::Engine
     include Motorhead::Engine
-    #active_if { rand(2).odd? }
-    active_if { false }
+    active_if { rand(2).odd? }
   end
 end
