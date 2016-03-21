632669ba80ecbd3500dd2fd9db9e0e46b2f5004c
matsuhisa <matsuhisa+github@gmail.com>
2016-03-22 00:20:18 +0900

    Add seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160227051331) do
+ActiveRecord::Schema.define(version: 20160321150634) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "book_authors", force: :cascade do |t|
+    t.integer  "book_id",    limit: 4
+    t.integer  "author_id",  limit: 4
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string   "name",        limit: 255
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,13 @@
 [
 { name: "イズムの功過", price: 1000 }, { name: "一夜", price: 1200 }, { name: "永日小品", price: 1500 }, { name: "薤露行", price: 800 }, { name: "学者と名誉", price: 500 }, { name: "硝子戸の中", price: 700 }, { name: "虚子君へ", price: 1000 }, { name: "草枕", price: 1200 }, { name: "虞美人草", price: 1500 }, { name: "ケーベル先生", price: 800 }, { name: "こころ", price: 500 }, { name: "三四郎", price: 700 },
 ].each_with_index do |book, index|
-  Book.create(name: book[:name], price: book[:price])
+  Book.create(id:index+1, name: book[:name], price: book[:price])
 end
+
+[{ name: "夏目漱石" }].each_with_index do |author, index|
+  Author.create(id:index+1, name: author[:name])
+end
+
+(1..12).each{|num|
+  BookAuthor.create(book_id: num, author_id: 1)
+}
